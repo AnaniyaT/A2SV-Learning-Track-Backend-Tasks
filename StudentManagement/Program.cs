de3fc19685e277f538f4e934ded2d47ef24f36ba
Ananiya <ananiyat24@gmail.com>
2023-08-04 16:47:11 +0300

    clean main

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -1,24 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using StudentManagement;
 
-Console.WriteLine("Hello, World!");
 
-// Student student = new Student(
-//         "Ananas",
-//         DepartmentType.Architecture,
-//         4,
-//         21,
-//         3.5
-//     );
-//
-// StudentList<Student> list = new StudentList<Student>();
-// list.LoadFromJson("something.json");
-// string json = JsonSerializer.Serialize(list);
-// Console.WriteLine(json);
-
-// Student? stu = JsonSerializer.Deserialize<Student>(json);
-    // Console.WriteLine(stu);
-    Cli cli = new Cli();
-    cli.Start();
+Cli cli = new Cli();
+cli.Start();
